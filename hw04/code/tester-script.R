1e56498ba4f3f2c39c66e76db955361371543c80
Jesse Gao <jessegao12@gmail.com>
2017-11-26 21:50:34 -0800

    done

--- hw04/code/tester-script.R
+++ hw04/code/tester-script.R
@@ -5,3 +5,6 @@ source('functions.R')
 sink('../output/test-reporter.txt')
 test_file('tests.R')
 sink()
+library(shiny)
+# Run an app from a subdirectory in the repo
+runGitHub("stat133-hws-fall17", "jessegao", subdir = "hw04/app")
